cdc95f262583ad16cc92acd698b745b46f9e2602
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2022-10-06 10:08:11 +0200

    cambios_git

---
+++ java_capitulo_1/Ejercicio2.java
@@ -0,0 +1,9 @@
+package java_capitulo_1;
+
+public class Ejercicio2 {
+  public static void main(String[] args) {
+    System.out.println("Joseph Luca San Juan Thomspson");
+    System.out.println("Av. de la Música, Nº 18");
+    System.out.println("660 60 02 62");
+  }
+}
---
+++ java_capitulo_1/Ejercicio4.java
@@ -0,0 +1,14 @@
+package java_capitulo_1;
+
+public class Ejercicio4 {
+  public static void main(String[] args) {
+    System.out.printf(" Lunes | Martes | Miércoles | Jueves | Viernes ");
+    System.out.printf("%15f| %15f| %15f| %15f| %15f|");
+    System.out.printf("%15f| %15f| %15f| %15f| %15f|");
+    System.out.printf("%15f| %15f| %15f| %15f| %15f|");
+    System.out.printf("");
+    System.out.printf("%15f| %15f| %15f| %15f| %15f|");
+    System.out.printf("%15f| %15f| %15f| %15f| %15f|");
+    System.out.printf("%15f| %15f| %15f| %15f| %15f|");
+  }
+}
