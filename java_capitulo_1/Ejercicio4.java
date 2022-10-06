d4430ac6053823169778e0585560df3b0a942288
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2022-10-06 10:17:22 +0200

    cambios a 4

--- java_capitulo_1/Ejercicio4.java
+++ java_capitulo_1/Ejercicio4.java
@@ -3,12 +3,12 @@ package java_capitulo_1;
 public class Ejercicio4 {
   public static void main(String[] args) {
     System.out.printf(" Lunes | Martes | Miércoles | Jueves | Viernes ");
-    System.out.printf("%15f| %15f| %15f| %15f| %15f|");
-    System.out.printf("%15f| %15f| %15f| %15f| %15f|");
-    System.out.printf("%15f| %15f| %15f| %15f| %15f|");
+    System.out.printf("%-15s| %-15s| %-15s| %-15s| %-15s|");
+    System.out.printf("%-15s| %-15s| %-15s| %-15s| %-15s|");
+    System.out.printf("%-15s| %-15s| %-15s| %-15s| %-15s|");
     System.out.printf("");
-    System.out.printf("%15f| %15f| %15f| %15f| %15f|");
-    System.out.printf("%15f| %15f| %15f| %15f| %15f|");
-    System.out.printf("%15f| %15f| %15f| %15f| %15f|");
+    System.out.printf("%-15s| %-15s| %-15s| %-15s| %-15s|");
+    System.out.printf("%-15s| %-15s| %-15s| %-15s| %-15s|");
+    System.out.printf("%-15s| %-15s| %-15s| %-15s| %-15s|");
   }
 }
